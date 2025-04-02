f31b0ba5f1b20c454082a4e1cadba20561c1fe18
olatejulian <julian.lf.olate@gmail.com>
2025-04-02 17:35:38 -0300

    test(core): :white_check_mark: refact test suite for new password vo

--- core/test/account/domain/value-object/Password.test.ts
+++ core/test/account/domain/value-object/Password.test.ts
@@ -1,68 +1,37 @@
 import {InvalidPasswordError, Password} from '@core/account'
 
 describe('Password Unit Tests', () => {
-    it('should be able to create a valid password', () => {
-        // given
-        const password = new Password('Password@123')
+    it('should be able to create a valid password', async () => {
+        const validPasswordString = 'Password@1234!'
 
-        // then
-        expect(password.getValue).toBeDefined()
+        const password = await Password.create(validPasswordString)
+
+        expect(password).toBeDefined()
     })
 
-    it('should throw an error when password is null', () => {
-        // given
-        const invalidPassword = null
+    it('should throw an error when password is invalid', async () => {
+        const invalidPasswordString = 'foo'
 
-        // when
-        const createInvalidPassword = () => new Password(invalidPassword)
-
-        // then
-        expect(createInvalidPassword).toThrow(InvalidPasswordError)
+        expect(async () => {
+            await Password.create(invalidPasswordString)
+        }).rejects.toThrow(InvalidPasswordError)
     })
 
-    it('should throw an error when password is undefined', () => {
-        // given
-        const invalidPassword = undefined
+    it('should be able to compare passwords', async () => {
+        const passwordString = 'Password@1234!'
 
-        // when
-        const createInvalidPassword = () => new Password(invalidPassword)
+        const password = await Password.create(passwordString)
 
-        // then
-        expect(createInvalidPassword).toThrow(InvalidPasswordError)
+        expect(await password.compare(passwordString)).toBe(true)
     })
 
-    it('should be able to throw an error when password is lower than 8 characters', () => {
-        // given
-        const invalidPassword = '1234567'
+    it('should be able to compare passwords that are not the same and return false', async () => {
+        const passwordString = 'Password@1234!'
 
-        // when
-        const createInvalidPassword = () => new Password(invalidPassword)
+        const password = await Password.create(passwordString)
 
-        // then
-        expect(createInvalidPassword).toThrow(InvalidPasswordError)
-    })
+        const differentPasswordString = 'DifferentPassword@1234!'
 
-    it('should be able to hash a string', async () => {
-        // given
-        const value = 'Password@123'
-        const password = new Password(value)
-
-        // when
-        await password.hash()
-
-        // then
-        expect(password.getValue == value).toBe(false)
-    })
-
-    it('should be able to compare a hashed password with a plain text password', async () => {
-        // given
-        const value = 'Password@123'
-        const password = new Password(value)
-
-        // when
-        await password.hash()
-
-        // then
-        expect(await password.compare(value)).toBe(true)
+        expect(await password.compare(differentPasswordString)).toBe(false)
     })
 })
