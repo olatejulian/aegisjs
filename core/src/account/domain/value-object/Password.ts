f53bca68fcc78ad62ed272359c00f6f0a25b87ea
olatejulian <julian.lf.olate@gmail.com>
2025-04-08 22:58:18 -0300

    refactor: :recycle: make password serialize possible

--- core/src/account/domain/value-object/Password.ts
+++ core/src/account/domain/value-object/Password.ts
@@ -3,58 +3,93 @@ import * as bcrypt from 'bcrypt'
 export class InvalidPasswordError extends Error {}
 
 export class Password {
+    private constructor(private readonly password: HashedPassword) {}
+
+    public static async fromPlainString(password: string): Promise<Password> {
+        const plainPassword = PlainPassword.fromPlainString(password)
+
+        const hashedPassword = await plainPassword.hash()
+
+        return new Password(hashedPassword)
+    }
+
+    public static fromHashedString(password: string): Password {
+        const state = HashedPassword.fromHashedString(password)
+
+        return new Password(state)
+    }
+
+    public toString(): string {
+        return this.password.toString()
+    }
+
+    public async compare(plainTextPassword: string): Promise<boolean> {
+        return await this.password.compare(plainTextPassword)
+    }
+}
+
+class PlainPassword {
     private static readonly MIN_LENGTH = 12
     private static readonly MAX_LENGTH = 64
     private static readonly SALT_ROUNDS = 10
 
     private constructor(private readonly password: string) {}
 
-    public static async create(password: string): Promise<Password> {
-        Password.validate(password)
+    public static fromPlainString(password: string): PlainPassword {
+        PlainPassword.validate(password)
 
+        return new PlainPassword(password)
+    }
+
+    public async hash(): Promise<HashedPassword> {
         const hashedPassword = await bcrypt.hash(
-            password,
-            Password.SALT_ROUNDS
+            this.password,
+            PlainPassword.SALT_ROUNDS
         )
 
-        return new Password(hashedPassword)
+        return HashedPassword.fromHashedString(hashedPassword)
+    }
+
+    private static validate(password: string) {
+        const passwordMustBeInLengthRange =
+            password.length >= PlainPassword.MIN_LENGTH &&
+            password.length <= PlainPassword.MAX_LENGTH
+
+        const passwordMustBeValidFormat =
+            /[A-Z]/.test(password) &&
+            /[a-z]/.test(password) &&
+            /\d/.test(password) &&
+            /[!@#$%^&*()_+{}\\[\]:;<>,.?~]/.test(password)
+
+        if (!(passwordMustBeInLengthRange && passwordMustBeValidFormat))
+            throw new InvalidPasswordError()
+    }
+}
+
+class HashedPassword {
+    private static readonly BCRYPT_HASH_REGEX = new RegExp(
+        /^\$2[aby]?\$\d{2}\$[./A-Za-z0-9]{53}$/,
+        'u'
+    )
+
+    private constructor(private readonly password: string) {}
+
+    public static fromHashedString(password: string): HashedPassword {
+        HashedPassword.validate(password)
+
+        return new HashedPassword(password)
+    }
+
+    public toString(): string {
+        return this.password
     }
 
     public async compare(plainTextPassword: string): Promise<boolean> {
         return await bcrypt.compare(plainTextPassword, this.password)
     }
 
-    private static validate(value: string): void {
-        if (
-            !Password.mustBeDefinedString(value) ||
-            !Password.passwordMustBeAtLeastXCharacters(value) ||
-            !Password.passwordMustBeAtMostXCharacters(value) ||
-            !Password.passwordMustBeValidFormat(value)
-        ) {
+    private static validate(password: string): void {
+        if (!HashedPassword.BCRYPT_HASH_REGEX.test(password))
             throw new InvalidPasswordError()
-        }
-    }
-
-    private static mustBeDefinedString(value: string): boolean {
-        return (
-            value !== undefined && value !== null && typeof value === 'string'
-        )
-    }
-
-    private static passwordMustBeAtLeastXCharacters(value: string): boolean {
-        return value.length >= Password.MIN_LENGTH
-    }
-
-    private static passwordMustBeAtMostXCharacters(value: string): boolean {
-        return value.length <= Password.MAX_LENGTH
-    }
-
-    private static passwordMustBeValidFormat(value: string): boolean {
-        return (
-            /[A-Z]/.test(value) &&
-            /[a-z]/.test(value) &&
-            /\d/.test(value) &&
-            /[!@#$%^&*()_+{}\\[\]:;<>,.?~]/.test(value)
-        )
     }
 }
