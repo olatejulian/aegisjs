c591496d0d53d11d7a5c7aa8e87c4de4f480c5c3
olatejulian <julian.lf.olate@gmail.com>
2025-04-08 23:01:03 -0300

    test(core): :white_check_mark: rename password methods

--- core/test/account/domain/value-object/Password.test.ts
+++ core/test/account/domain/value-object/Password.test.ts
@@ -4,7 +4,7 @@ describe('Password Unit Tests', () => {
     it('should be able to create a valid password', async () => {
         const validPasswordString = 'Password@1234!'
 
-        const password = await Password.create(validPasswordString)
+        const password = await Password.fromPlainString(validPasswordString)
 
         expect(password).toBeDefined()
     })
@@ -13,25 +13,25 @@ describe('Password Unit Tests', () => {
         const invalidPasswordString = 'foo'
 
         expect(async () => {
-            await Password.create(invalidPasswordString)
+            await Password.fromPlainString(invalidPasswordString)
         }).rejects.toThrow(InvalidPasswordError)
     })
 
     it('should be able to compare passwords', async () => {
         const passwordString = 'Password@1234!'
 
-        const password = await Password.create(passwordString)
+        const password = await Password.fromPlainString(passwordString)
 
-        expect(await password.compare(passwordString)).toBe(true)
+        expect(await password.compare(passwordString)).toBeTruthy()
     })
 
     it('should be able to compare passwords that are not the same and return false', async () => {
         const passwordString = 'Password@1234!'
 
-        const password = await Password.create(passwordString)
+        const password = await Password.fromPlainString(passwordString)
 
         const differentPasswordString = 'DifferentPassword@1234!'
 
-        expect(await password.compare(differentPasswordString)).toBe(false)
+        expect(await password.compare(differentPasswordString)).toBeFalsy()
     })
 })
