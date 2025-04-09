2bc5176b4d28f8495442a6eb54362ea7624b54ae
olatejulian <julian.lf.olate@gmail.com>
2025-04-08 23:10:24 -0300

    test: :white_check_mark: add e-mail verification token vo test suit

---
+++ core/test/account/domain/value-object/EmailVerificationToken.test.ts
@@ -0,0 +1,51 @@
+import {
+    EmailVerificationToken,
+    InvalidEmailVerificationTokenError,
+} from '@core/account'
+import {validate} from 'uuid'
+
+describe('Email Verification Token Unit Tests', () => {
+    it('Should be able to create a valid email verification token', () => {
+        const emailVerificationToken = EmailVerificationToken.generateToken()
+
+        expect(emailVerificationToken).toBeDefined()
+
+        expect(validate(emailVerificationToken.toString())).toBeTruthy()
+    })
+
+    it('Should throw an error when email verification token is invalid', () => {
+        expect(() =>
+            EmailVerificationToken.fromString('invalid-token')
+        ).toThrow(InvalidEmailVerificationTokenError)
+    })
+
+    it('Should be able to compare with another email verification token', () => {
+        const emailVerificationTokenA = EmailVerificationToken.generateToken()
+
+        const emailVerificationTokenB = EmailVerificationToken.generateToken()
+
+        const emailVerificationTokenC = EmailVerificationToken.fromString(
+            emailVerificationTokenA.toString()
+        )
+
+        expect(
+            emailVerificationTokenA.equals(emailVerificationTokenB)
+        ).toBeFalsy()
+
+        expect(
+            emailVerificationTokenB.equals(emailVerificationTokenA)
+        ).toBeFalsy()
+
+        expect(
+            emailVerificationTokenA.equals(emailVerificationTokenC)
+        ).toBeTruthy()
+
+        expect(
+            emailVerificationTokenC.equals(emailVerificationTokenA)
+        ).toBeTruthy()
+
+        expect(
+            emailVerificationTokenB.equals(emailVerificationTokenC)
+        ).toBeFalsy()
+    })
+})
