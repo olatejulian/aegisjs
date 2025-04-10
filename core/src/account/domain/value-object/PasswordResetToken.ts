5b9a586eacc503b07ed2c1958e55d349c5fe322a
olatejulian <julian.lf.olate@gmail.com>
2025-04-10 18:51:50 -0300

    feat: :sparkles: add password reset token vo

---
+++ core/src/account/domain/value-object/PasswordResetToken.ts
@@ -0,0 +1,29 @@
+import {v4 as uuidv4, validate} from 'uuid'
+
+export class InvalidPasswordResetTokenError extends Error {}
+
+export class PasswordResetToken {
+    private constructor(private readonly token: string) {}
+
+    public static generateToken(): PasswordResetToken {
+        return new PasswordResetToken(uuidv4())
+    }
+
+    public static fromString(token: string): PasswordResetToken {
+        PasswordResetToken.validate(token)
+
+        return new PasswordResetToken(token)
+    }
+
+    public toString(): string {
+        return this.token
+    }
+
+    public equals(other: PasswordResetToken): boolean {
+        return this.toString() === other.toString()
+    }
+
+    private static validate(token: string): void {
+        if (!validate(token)) throw new InvalidPasswordResetTokenError()
+    }
+}
