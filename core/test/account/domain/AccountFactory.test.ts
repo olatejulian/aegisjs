7d17dbe2cd52a94a1085b2dfd69e139267bd3f26
olatejulian <julian.lf.olate@gmail.com>
2025-04-22 20:08:15 -0300

    test: :white_check_mark: add account factory unit tests

---
+++ core/test/account/domain/AccountFactory.test.ts
@@ -0,0 +1,27 @@
+import {Account, AccountFactory} from '@core/account'
+
+describe('Account Factory Unit Tests', () => {
+    it('should be able to create an account from primitives values', async () => {
+        const accountNameString = 'John Doe'
+
+        const emailAddressString = 'john.doe@email.com'
+
+        const passwordString = 'MyPasswordWith!@#And123'
+
+        const account = await AccountFactory.createAccount({
+            name: accountNameString,
+            emailAddress: emailAddressString,
+            password: passwordString,
+        })
+
+        expect(account).toBeDefined()
+
+        expect(account).toBeInstanceOf(Account)
+
+        expect(account.getName().toString()).toBe(accountNameString)
+
+        expect(account.getEmailAddress().toString()).toBe(emailAddressString)
+
+        expect(account.verifyPlainPassword(passwordString)).toBeTruthy()
+    })
+})
