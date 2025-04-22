01051bfce742a65202cf5d736c45580ce015d4f8
olatejulian <julian.lf.olate@gmail.com>
2025-04-22 20:21:23 -0300

    refactor: :truck: move packages to packages dir

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,13 @@ import type {Config} from 'jest'
 const config: Config = {
     preset: 'ts-jest',
     projects: [
-        '<rootDir>/app/jest.config.ts',
-        '<rootDir>/core/jest.config.ts',
+        '<rootDir>/packages/app/jest.config.ts',
+        '<rootDir>/packages/core/jest.config.ts',
     ],
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'js', 'json'],
     testMatch: ['**/*.spec.ts', '**/*.test.ts'],
-    collectCoverageFrom: ['<rootDir>/app/**/*.ts', '<rootDir>/core/**/*.ts'],
+    collectCoverageFrom: ['<rootDir>/packages/app/**/*.ts', '<rootDir>/packages/core/**/*.ts'],
     coverageDirectory: '<rootDir>/coverage/',
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
