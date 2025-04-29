fe8e6ac7ea568b0bd4bbb77dafe051109d54d75c
olatejulian <julian.lf.olate@gmail.com>
2025-04-29 16:40:34 -0300

    feat(app): :sparkles: add decorator to simplify controller

---
+++ packages/app/src/account/account-signup-post.decorator.ts
@@ -0,0 +1,31 @@
+import {applyDecorators, HttpStatus, Post} from '@nestjs/common'
+import {ApiBody, ApiResponse, ApiTags} from '@nestjs/swagger'
+import {
+    CreateAccount201Response,
+    CreateAccount400Response,
+    CreateAccount422Response,
+} from './account.response'
+import {CreateAccountDto} from './dto'
+
+export function AccountSignupPost() {
+    return applyDecorators(
+        Post('signup'),
+        ApiTags('Public Routes'),
+        ApiBody({type: CreateAccountDto}),
+        ApiResponse({
+            status: HttpStatus.CREATED,
+            description: 'Account created successfully',
+            type: CreateAccount201Response,
+        }),
+        ApiResponse({
+            status: HttpStatus.BAD_REQUEST,
+            description: 'Oh, no! Something went wrong',
+            type: CreateAccount400Response,
+        }),
+        ApiResponse({
+            status: HttpStatus.UNPROCESSABLE_ENTITY,
+            description: 'Schema validation failed',
+            type: CreateAccount422Response,
+        })
+    )
+}
