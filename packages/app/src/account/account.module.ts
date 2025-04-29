78097318a7be3a62a72b667d38eaf95bbe1224cb
olatejulian <julian.lf.olate@gmail.com>
2025-04-29 16:41:48 -0300

    refactor: :recycle: remove unused dependencies

--- packages/app/src/account/account.module.ts
+++ packages/app/src/account/account.module.ts
@@ -1,41 +1,10 @@
-import {CACHE_MANAGER} from '@nestjs/cache-manager'
 import {Module} from '@nestjs/common'
-import {ConfigService} from '@nestjs/config'
-import {JwtModule, JwtService} from '@nestjs/jwt'
-import {
-    AccountRepository,
-    InMemoryAccountRepository,
-} from '@ts-api-example/core'
-import {Cache} from 'cache-manager'
 import {AccountController} from './account.controller'
 import {AccountService} from './account.service'
+import {InMemoryAccountRepository} from './repository'
 
 @Module({
-    imports: [JwtModule.register({})],
     controllers: [AccountController],
-    providers: [
-        {
-            provide: AccountService,
-            useFactory: (
-                repository: AccountRepository,
-                jwtService: JwtService,
-                configService: ConfigService,
-                cacheService: Cache
-            ) =>
-                new AccountService(
-                    repository,
-                    jwtService,
-                    configService,
-                    cacheService
-                ),
-            inject: [
-                InMemoryAccountRepository,
-                JwtService,
-                ConfigService,
-                CACHE_MANAGER,
-            ],
-        },
-        InMemoryAccountRepository,
-    ],
+    providers: [AccountService, InMemoryAccountRepository],
 })
 export class AccountModule {}
