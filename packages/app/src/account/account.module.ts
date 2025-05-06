3dec420829b15307d3325a329b82813cb613bfc4
olatejulian <julian.lf.olate@gmail.com>
2025-05-06 20:28:19 -0300

    refactor: :recycle: fix account dependency injection

--- packages/app/src/account/account.module.ts
+++ packages/app/src/account/account.module.ts
@@ -1,10 +1,19 @@
 import {Module} from '@nestjs/common'
+import {AccountRepository} from '@ts-api-example/core'
 import {AccountController} from './account.controller'
 import {AccountService} from './account.service'
 import {InMemoryAccountRepository} from './repository'
 
 @Module({
     controllers: [AccountController],
-    providers: [AccountService, InMemoryAccountRepository],
+    providers: [
+        {
+            provide: AccountService,
+            useFactory: (repository: AccountRepository) =>
+                new AccountService(repository),
+            inject: [InMemoryAccountRepository],
+        },
+        InMemoryAccountRepository,
+    ],
 })
 export class AccountModule {}
