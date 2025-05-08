8900db1a6fcc0b0c6e811a223519151931331e8c
olatejulian <julian.lf.olate@gmail.com>
2025-05-08 16:42:36 -0300

    chore: :wrench: use pattern to find more packages

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,11 @@ import type {Config} from 'jest'
 
 const config: Config = {
     preset: 'ts-jest',
-    projects: [
-        '<rootDir>/packages/app/jest.config.ts',
-        '<rootDir>/packages/core/jest.config.ts',
-    ],
+    projects: ['<rootDir>/packages/**/jest.config.ts'],
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'js', 'json'],
     testMatch: ['**/*.spec.ts', '**/*.test.ts'],
-    collectCoverageFrom: ['<rootDir>/packages/app/**/*.ts', '<rootDir>/packages/core/**/*.ts'],
+    collectCoverageFrom: ['<rootDir>/packages/**/*.ts'],
     coverageDirectory: '<rootDir>/coverage/',
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
