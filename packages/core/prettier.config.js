8d383b98b142d9229231dc2e93f6f8def4867f7a
olatejulian <julian.lf.olate@gmail.com>
2025-07-31 00:04:07 -0300

    chore: :wrench: change ts config files for their js version

---
+++ packages/core/prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+    arrowParens: 'avoid',
+    bracketSpacing: false,
+    printWidth: 80,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+}
