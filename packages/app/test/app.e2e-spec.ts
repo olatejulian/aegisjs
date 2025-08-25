6f7135dd7d504fa3eb6d2947d3baf2b9224843ff
olatejulian <julian.lf.olate@gmail.com>
2025-08-24 23:41:36 -0300

    test: :white_check_mark: fix test expects

--- packages/app/test/app.e2e-spec.ts
+++ packages/app/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
+import {AppModule} from '@app/app.module'
 import {INestApplication} from '@nestjs/common'
 import {Test, TestingModule} from '@nestjs/testing'
 import * as request from 'supertest'
-import {AppModule} from '../src/app.module'
 
 describe('AppController (e2e)', () => {
     let app: INestApplication
@@ -15,10 +15,9 @@ describe('AppController (e2e)', () => {
         await app.init()
     })
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!')
+    it('/ (GET)', async () => {
+        const response = await request(app.getHttpServer()).get('/')
+        expect(response.status).toBe(200)
+        expect(response.text).toBe('Hello World!')
     })
 })
