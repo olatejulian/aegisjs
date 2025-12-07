89bb5b93acedb993eed45293580a0caf193bd4da
olatejulian <julian.lf.olate@gmail.com>
2025-12-06 23:13:49 -0300

    feat: :sparkles: :construction: add mail modules

--- packages/app/src/account/account.module.ts
+++ packages/app/src/account/account.module.ts
@@ -1,10 +1,17 @@
+import {MailModule, MailService} from '@app/mail'
+import {
+    TemplateRendererModule,
+    TemplateRendererService,
+} from '@app/template-renderer'
 import {Module} from '@nestjs/common'
 import {AccountRepository} from '@ts-api-example/core'
 import {AccountController} from './account.controller'
 import {AccountService} from './account.service'
+import {AccountCreatedEventListener} from './listener'
 import {InMemoryAccountRepository} from './repository'
 
 @Module({
+    imports: [MailModule, TemplateRendererModule],
     controllers: [AccountController],
     providers: [
         {
@@ -13,7 +20,10 @@ import {InMemoryAccountRepository} from './repository'
                 new AccountService(repository),
             inject: [InMemoryAccountRepository],
         },
+        AccountCreatedEventListener,
         InMemoryAccountRepository,
+        MailService,
+        TemplateRendererService,
     ],
 })
 export class AccountModule {}
---
+++ packages/app/src/account/listener/account-created-event.listener.ts
@@ -0,0 +1,39 @@
+import {Mail, MailService} from '@app/mail'
+import {TemplateRendererService} from '@app/template-renderer'
+import {Injectable} from '@nestjs/common'
+import {OnEvent} from '@nestjs/event-emitter'
+import {AccountCreatedEvent} from '@ts-api-example/core'
+
+@Injectable()
+export class AccountCreatedEventListener {
+    constructor(
+        private readonly mailService: MailService,
+        private readonly templateRendererService: TemplateRendererService
+    ) {}
+
+    @OnEvent(AccountCreatedEvent.EVENT_NAME)
+    public async sendVerificationEmail(event: AccountCreatedEvent) {
+        const {name, email, token} = event.payload()
+
+        const verifyUrl = `http://localhost:3000/verify-email/${token}`
+
+        const template = await this.templateRendererService.render({
+            templateName: 'verification-email-address',
+            data: {
+                appName: 'TS-API-Example',
+                name: name,
+                verifyUrl: verifyUrl,
+                year: new Date().getFullYear(),
+            },
+        })
+
+        const mail: Mail = {
+            to: email,
+            subject: `Hello ${name}`,
+            text: 'Hello world',
+            html: template,
+        }
+
+        await this.mailService.sendEmail(mail)
+    }
+}
